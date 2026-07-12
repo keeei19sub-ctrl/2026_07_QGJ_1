869034408b32298c486700743b2e9f20b69d2529
cocoroskk <cocosakaki556@gmail.com>
2026-07-12 16:03:12 +0900

    ?

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIHandler : MonoBehaviour
+{
+   	private VisualElement m_Healthbar;
+   	public static UIHandler instance { get; private set; }
+
+   	// Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
+   	private void Awake()
+   	{
+       	instance = this;
+   	}
+
+   	// Start is called once before the first execution of Update after the MonoBehaviour is created 
+   	void Start()
+   	{
+       	UIDocument uiDocument = GetComponent<UIDocument>();
+       	m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
+       	SetHealthValue(1.0f);
+   	}
+
+   	public void SetHealthValue(float percentage)
+   	{
+       	m_Healthbar.style.width = Length.Percent(100 * percentage);
+   	}
+}
