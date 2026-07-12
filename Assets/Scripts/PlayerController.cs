3d9a0fc1f9ff1ca626737660d69ec844125a2f02
cocoroskk <cocosakaki556@gmail.com>
2026-07-12 18:19:49 +0900

    Visualize shop interaction ray

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    public InputAction BuyItem;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    Vector2 rayDirection = Vector2.right;
     public float Speed = 10.0f;
     float maxCordinateXPlus = 4.6f;
     float maxCordinateXMinus = -2.7f;
@@ -16,12 +18,14 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         MoveAction.Enable();
+        BuyItem.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+        buyItem();
         parasolDirectionChange();
         parasolChange();
     }
@@ -67,6 +71,36 @@ public class PlayerController : MonoBehaviour
             case 0:
                 // どちらも無効のまま
                 break;
+        }
+    }
+
+    void buyItem()
+    {
+        const float rayDistance = 5f;
+        if (move.sqrMagnitude > 0.001f)
+        {
+            rayDirection = move.normalized;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            rigidbody2d.position,
+            rayDirection,
+            rayDistance,
+            LayerMask.GetMask("Shop"));
+
+        Color rayColor = hit.collider != null ? Color.green : Color.red;
+        Debug.DrawRay(rigidbody2d.position, rayDirection * rayDistance, rayColor);
+
+        if(hit.collider != null)
+        {
+            FindShop(hit);
+        }
+    }
+    void FindShop(RaycastHit2D hit)
+    {
+        if (BuyItem.WasPressedThisFrame())
+        {
+            Debug.Log("raycast hit" + hit.collider.gameObject);
         }
     }
 }
